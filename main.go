e89c7da43f724b26483034ea9c8f8fc05218577e
Ryan Bigg <me@ryanbigg.com>
2019-06-12 11:49:59 +1000

    Specify version

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/radar/setup/installer"
 	"github.com/urfave/cli"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+
+	fmt.Println(fmt.Sprintf("%v, commit %v, built at %v", version, commit, date))
 	app := cli.NewApp()
 	app.Name = "setup"
 	app.Usage = "Setup your local development environment"
